c56ad2c9bd9e0cbec4d4db66899ced606e8e184d
henry.leinen@online.de <henry.leinen@online.de>
2018-03-26 17:59:19 +0200

    Added keyboard interpretation class.

--- mcdu/display.py
+++ mcdu/display.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from Tkinter import *
+from keyboard import keyboard
 import tkFont
 import time
 
@@ -8,32 +9,6 @@ bigsize = 38
 smlsize = 20
 half_spacing = smlsize+5
 full_spacing = 112
-
-KEY_CLR = 20
-KEY_DEL = 21
-KEY_DIR = 100
-KEY_PROG= 101
-KEY_PERF= 102
-KEY_INIT= 103
-KEY_DATA= 104
-KEY_F_PLN=110
-KEY_RAD_NAV=111
-KEY_FUEL_PRED=112
-KEY_SEC_F_PLN=113
-KEY_ATC_COMM=114
-KEY_MENU= 115
-KEY_AIRPORT=120
-KEY_PAGE_UP=121
-KEY_PAGE_DN=122
-KEY_NEXT_PAGE=130
-KEY_LSK1L = 1
-KEY_LSK6L = 6
-KEY_LSK1R = 11
-KEY_LSK6R = 16
-KEY_A = 65
-KEY_Z = ord('Z')
-KEY_0 = 48
-KEY_9 = ord('9')
 
 rowt = 5
 row0 = rowt + bigsize
@@ -66,13 +41,14 @@ class myDisplay(object):
 		self.fnt_big = tkFont.Font(family='AirbusMCDUa', size=bigsize)
 		self.fnt_sml = tkFont.Font(family='AirbusMCDUa', size=smlsize)
 
-
 	# required method, called from main
 	def initialize(self, mcdu):
 		self.LOOP_ACTIVE = True
 		print "Initializing"
 		self.createWidgets()
 		self.mcdu = mcdu
+		self.keyboard = keyboard()
+		self.keyboard.open()
 
 	# required method, called from main
 	def open(self):
@@ -80,7 +56,6 @@ class myDisplay(object):
 
 	# unused method, not called hopefully --> instead everything done via messageloop
 	def on_message(self, message):
-		print "Unused method 'on_message' called"
 		if message == "DEL":
 			self.mcdu.scratch_delete()
 		elif message == "CLR":
@@ -105,9 +80,10 @@ class myDisplay(object):
 
 	# required method
 	def on_close(self):
+		self.LOOP_ACTIVE = False
 		self.mcdu.remove_display(self)
 		self.root.destroy()
-		self.LOOP_ACTIVE = False
+#		self.keyboard.close()
 		print "Exiting"
 
 	def on_click(self, event):
@@ -179,66 +155,15 @@ class myDisplay(object):
 		self.w.create_text( colr, row5, text=" ", font=self.fnt_sml, fill="#ffffff", tags="LSK5R", anchor=NE)
 		self.w.create_text( colr, row5s,text=" ", font=self.fnt_big, fill="#ffffff", tags="LSK5Rs",anchor=NE)
 
-
-	def parseInput(self, byte):
-		print "Received byte "+ byte
-		cmd = ord(byte)
-		if cmd >= KEY_LSK1L and cmd <= KEY_LSK6L:
-			num = cmd-1
-			side = 0
-			print "KEY: LSK"+str(num)+"L"
-			self.mcdu.lsk((num,side))
-		elif cmd >= KEY_LSK1R and cmd <= KEY_LSK6R:
-			num = cmd - 11
-			side = 1
-			print "KEY: LSK"+str(num)+"R"
-			self.mcdu.lsk((num,side))
-		elif cmd == KEY_CLR:
-			print "KEY: CLR"
-			self.mcdu.scratch_clear()
-		elif cmd == KEY_DEL:
-			print "KEY: DEL"
-			self.mcdu.scratch_delete()
-		elif cmd == KEY_MENU:
-			print "KEY: MENU"
-			self.mcdu.menu()
-		elif cmd == KEY_DIR:
-			print "KEY: DIR"
-			self.mcdu.dir()
-		elif cmd == KEY_PROG:
-			print "KEY: PROG"
-			self.mcdu.prog()
-		elif cmd == KEY_F_PLN:
-			print "KEY: F-PLN"
-			self.mcdu.f_pln()
-		elif cmd == KEY_PERF:
-			print "KEY: PERF"
-			self.mcdu.perf()
-		elif cmd == KEY_INIT:
-			print "KEY: INIT"
-			self.mcdu.init()
-		elif cmd == KEY_DATA:
-			print "KEY: DATA"
-			self.mcdu.data()
-		elif cmd >= KEY_A and cmd <= KEY_Z:
-			self.mcdu.scratch_input(byte)
-		elif cmd >= KEY_0 and cmd <= KEY_9:
-			self.mcdu.scratch_input(byte)
-		else:
-			print "Byte received " + byte
-
 	def mainloop(self):
 		print "Entering Mainloop"
 		self.LOOP_ACTIVE = True
-		with open("/dev/kbdscan", "ro") as f:
-			while self.LOOP_ACTIVE:
-				self.root.update()
-				byte = f.read(1)
-				if byte != "":
-					self.parseInput(byte)
+		while self.LOOP_ACTIVE:
+			self.root.update()
+			message = self.keyboard.read()
+			if message != "":
+				self.on_message(message)
 		print "Leaving Mainloop"
 
 
 
-# m = myDisplay()
-# m.mainloop()
---
+++ mcdu/keyboard.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+class keyboard:
+
+	def interpret_char(self, char):
+		cmd = ord(char)
+		if cmd in self.keymap:
+			message = self.keymap[cmd]
+		else:
+			message = char
+		return message
+
+	def open(self):
+		self.file = open("/dev/kbdscan","ro")
+		try:
+			from configparser import SafeConfigParser
+		except ImportError:
+			from ConfigParser import SafeConfigParser
+		config = SafeConfigParser()
+		config.read("config/defaults.cfg")
+		config.read("~/.config/mcdu.cfg")
+		config.read("config/mcdu.cfg")
+		self.keymap = {}
+		self.keymap[config.getint("KeyMap", "KEY_CLR")] = "CLR"
+		self.keymap[config.getint("KeyMap", "KEY_DEL")] = "DEL"
+		self.keymap[config.getint("KeyMap", "KEY_DIR")] = "DIR"
+		self.keymap[config.getint("KeyMap", "KEY_PROG")] = "PROG"
+		self.keymap[config.getint("KeyMap", "KEY_PERF")] = "PERF"
+		self.keymap[config.getint("KeyMap", "KEY_INIT")] = "INIT"
+		self.keymap[config.getint("KeyMap", "KEY_DATA")] = "DATA"
+		self.keymap[config.getint("KeyMap", "KEY_F_PLN")] = "F_PLN"
+		self.keymap[config.getint("KeyMap", "KEY_RAD_NAV")] = "RAD_NAV"
+		self.keymap[config.getint("KeyMap", "KEY_FUEL_PRED")] = "FUEL_PRED"
+		self.keymap[config.getint("KeyMap", "KEY_SEC_F_PLN")] = "SEC_F_PLN"
+		self.keymap[config.getint("KeyMap", "KEY_ATC_COMM")] = "ATC_COMM"
+		self.keymap[config.getint("KeyMap", "KEY_MENU")] = "MENU"
+		self.keymap[config.getint("KeyMap", "KEY_DIM")] = "DIM"
+		self.keymap[config.getint("KeyMap", "KEY_AIRPORT")] = "AIRPORT"
+		self.keymap[config.getint("KeyMap", "KEY_PAGE_UP")] = "PAGE_UP"
+		self.keymap[config.getint("KeyMap", "KEY_NEXT_PAGE")] = "NEXT_PAGE"
+		self.keymap[config.getint("KeyMap", "KEY_PAGE_DN")] = "PAGE_DN"
+		self.keymap[config.getint("KeyMap", "KEY_LSK1L"  )] = "LSK0L"
+		self.keymap[config.getint("KeyMap", "KEY_LSK2L"  )] = "LSK1L"
+		self.keymap[config.getint("KeyMap", "KEY_LSK3L"  )] = "LSK2L"
+		self.keymap[config.getint("KeyMap", "KEY_LSK4L"  )] = "LSK3L"
+		self.keymap[config.getint("KeyMap", "KEY_LSK5L"  )] = "LSK4L"
+		self.keymap[config.getint("KeyMap", "KEY_LSK6L"  )] = "LSK5L"
+		self.keymap[config.getint("KeyMap", "KEY_LSK1R"  )] = "LSK0R"
+		self.keymap[config.getint("KeyMap", "KEY_LSK2R"  )] = "LSK1R"
+		self.keymap[config.getint("KeyMap", "KEY_LSK3R"  )] = "LSK2R"
+		self.keymap[config.getint("KeyMap", "KEY_LSK4R"  )] = "LSK3R"
+		self.keymap[config.getint("KeyMap", "KEY_LSK5R"  )] = "LSK4R"
+		self.keymap[config.getint("KeyMap", "KEY_LSK6R"  )] = "LSK5R"
+
+
+	def close(self):
+		self.file.close()
+
+	def read(self):
+		byte = self.file.read(1)
+		if byte != "":
+			print "Received char "+byte+" KeyCode:" + str(ord(byte))
+			message = self.interpret_char(byte)
+			if not message:
+				message=byte
+			print "Maps to message "+message
+		else:
+			message = ""
+		return message
+
+
